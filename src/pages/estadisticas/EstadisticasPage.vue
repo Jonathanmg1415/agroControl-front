197d5e6623a2d68d7a62bf28d05d4c27ab9c2ed8
Jonathanmg1415 <53263709+Jonathanmg1415@users.noreply.github.com>
2024-05-23 11:08:09 -0500

    arreglo de aspecto

--- src/pages/estadisticas/EstadisticasPage.vue
+++ src/pages/estadisticas/EstadisticasPage.vue
@@ -2,85 +2,78 @@
   <q-page padding>
     <q-card>
       <q-card-section class="row q-col-gutter-sm">
-        <q-card class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="row q-col-gutter-sm">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12  q-pa-sm">
-              <q-input
-                filled
-                v-model="startDateDisplay"
-                mask="date"
-                label="Fecha inicial "
-              >
-                <template v-slot:append>
-                  <q-icon
-                    name="event"
-                    class="cursor-pointer"
-                    @click="showStartDatePicker = true"
-                  />
-                </template>
-              </q-input>
-              <q-popup-proxy
-                v-model="showStartDatePicker"
-                transition-show="scale"
-                transition-hide="scale"
-                anchor="bottom right"
-                self="top right"
-              >
-                <q-date
-                  :locale="fechaEs"
-                  v-model="fechaInicial"
-                  minimal
-                  @update:model-value="updateStartDate"
-                >
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Cerrar" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12  q-pa-sm">
-              <q-input
-                filled
-                v-model="endDateDisplay"
-                mask="date"
-                label="Fecha final"
-              >
-                <template v-slot:append>
-                  <q-icon
-                    name="event"
-                    class="cursor-pointer"
-                    @click="showEndDatePicker = true"
-                  />
-                </template>
-              </q-input>
-              <q-popup-proxy
-                v-model="showEndDatePicker"
-                transition-show="scale"
-                transition-hide="scale"
-                anchor="bottom right"
-                self="top right"
-              >
-                <q-date
-                  :locale="fechaEs"
-                  v-model="fechaFinal"
-                  minimal
-                  @update:model-value="updateEndDate"
-                >
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Cerrar" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </div>
-            <div class="col-12  q-pa-sm">
-              <q-btn
-                label="Aplicar filtro"
-                @click="applyFilter"
-                color="primary"
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 q-pa-sm">
+          <q-input
+            filled
+            v-model="startDateDisplay"
+            mask="date"
+            label="Fecha inicial "
+          >
+            <template v-slot:append>
+              <q-icon
+                name="event"
+                class="cursor-pointer"
+                @click="showStartDatePicker = true"
               />
-            </div>
-          </div>
-        </q-card>
+            </template>
+          </q-input>
+          <q-popup-proxy
+            v-model="showStartDatePicker"
+            transition-show="scale"
+            transition-hide="scale"
+            anchor="bottom right"
+            self="top right"
+          >
+            <q-date
+              :locale="fechaEs"
+              v-model="fechaInicial"
+              minimal
+              @update:model-value="updateStartDate"
+            >
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Cerrar" color="primary" flat />
+              </div>
+            </q-date>
+          </q-popup-proxy>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 q-pa-sm">
+          <q-input
+            filled
+            v-model="endDateDisplay"
+            mask="date"
+            label="Fecha final"
+          >
+            <template v-slot:append>
+              <q-icon
+                name="event"
+                class="cursor-pointer"
+                @click="showEndDatePicker = true"
+              />
+            </template>
+          </q-input>
+          <q-popup-proxy
+            v-model="showEndDatePicker"
+            transition-show="scale"
+            transition-hide="scale"
+            anchor="bottom right"
+            self="top right"
+          >
+            <q-date
+              :locale="fechaEs"
+              v-model="fechaFinal"
+              minimal
+              @update:model-value="updateEndDate"
+            >
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Cerrar" color="primary" flat />
+              </div>
+            </q-date>
+          </q-popup-proxy>
+        </div>
+        <div class="col-12 q-pa-sm text-right q-mt-md">
+          <q-btn label="Aplicar filtro" @click="applyFilter" color="primary" />
+        </div>
+
         <div class="col-6 col-md-6">
           <q-card>
             <diagrama-barras :startDate="fechaInicial" :endDate="fechaFinal" />
@@ -99,7 +92,7 @@
                     />
                     <div class="text-subtitle1 col-auto">Total Egresos</div>
                   </div>
-                  <div class=" text-h6 ">
+                  <div class="text-h6">
                     ${{ totalEgresos.toLocaleString("es-CO") }}
                   </div>
                 </q-card-section>
@@ -140,8 +133,7 @@
 import { ref, watch, onMounted, computed } from "vue";
 import { defineAsyncComponent } from "vue";
 import { date } from "quasar";
-import { useEstadisticasStore } from 'stores/estadisticas-store';
-
+import { useEstadisticasStore } from "stores/estadisticas-store";
 
 const store = useEstadisticasStore();
 onMounted(() => {
@@ -192,8 +184,6 @@ const showEndDatePicker = ref(false);
 
 const startDateDisplay = ref(date.formatDate(fechaInicial.value, "YYYY-MM-DD"));
 const endDateDisplay = ref(date.formatDate(fechaFinal.value, "YYYY-MM-DD"));
-
-
 
 const updateStartDate = (value) => {
   startDateDisplay.value = date.formatDate(value, "YYYY-MM-DD");
